629743b799622e3b893cc4ee0df45ce7293814e0
Student-1386466 <55232695+Student-1386466@users.noreply.github.com>
2020-01-23 12:12:31 +0000

    Add files via upload

---
+++ pattern/if-statement-test/if-statement-test/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_statement_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Add num 1 ");
+            string one = Console.ReadLine();
+
+            if (one == "3")
+            {
+                Console.WriteLine("Correct");
+
+            }
+            else
+            {
+                Console.WriteLine("Game Over");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            Console.Write("\nAdd num 2 ");
+            string two = Console.ReadLine();
+
+            if (two == "3")
+            {
+                Console.WriteLine("Correct");
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Game Over");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ pattern/pattern recog 23-01-20/pattern recog 23-01-20/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattern_recog_23_01_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWelcome to 'MATHsQUIZ'");
+
+            Console.WriteLine("\nLets Start the GAME... ...");
+
+            Console.WriteLine("/nQ-01 : What is the square of 6?" +
+                " Your Options are ::" +
+                "  a) 12" +
+                "  b) 36");
+            string ansOne = Console.ReadLine();
+           
+
+            if (ansOne == "36")
+          
+            {
+                Console.WriteLine("Correct Answer.... Your Score is 1");
+                Console.WriteLine("Q-02 : What is 9*8=" +
+                    " Your Options are ::" +
+                    "  a) 81" +
+                    "  b) 72");
+                string ansTwo = Console.ReadLine();
+                
+                if( ansTwo == "72")
+                {
+                    Console.WriteLine("Correct Answer... Your Score is 2");
+                    Console.WriteLine("Q-03 : What is 50 / 5 =" +
+                        " Your Options are ::" +
+                        "  a) 10" +
+                        "  b) 9");
+                    string ansThree = Console.ReadLine();
+                    if( ansThree == "10")
+                    {
+                       Console.WriteLine("Correct Answer.... Your score is 3");
+                        Console.WriteLine("Q-04 : what is 129 - 83=" +
+                            " Your Options are ::" +
+                            "  a) 46" +
+                            "  b) 56");
+                        string ansFour = Console.ReadLine();
+                        if (ansFour == "46")
+                        {
+                            Console.WriteLine("Correct Answer ... Your Score is 4");
+                            Console.WriteLine("Q-05 : What is 64 + 98 =" +
+                                " Your Options are ::" +
+                                "  a) 162" +
+                                "  b) 172");
+                            string ansFive = Console.ReadLine();
+                            if (ansFive == "162")
+                            {
+                                Console.WriteLine("Correct Answer .... Your Score is 5");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Incorrect Answer.... GAME OVER.... Your Score is 4.... Better Luck Next Time");
+                            }
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Incorrect Answer.... GAME OVER.... Your Score is 3.... Better Luck Next Time");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect Answer.... GAME OVER.... Your Score is 2.... Better Luck Next Time");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Answer.... GAME OVER.... Your Score is 1.... Better Luck Next Time");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect Answer.... GAME OVER.... Your Score is 0.... Better Luck Next Time");
+                Console.ReadKey();
+            }
+        }
+    }
+}
